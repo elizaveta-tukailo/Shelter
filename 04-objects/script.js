22fe93b804fc8a2496ea3216efa0ab6095b52e21
elizaveta-tukailo <liza_tukailo@mail.ru>
2021-04-06 22:32:06 +0300

    feat: task4

---
+++ 04-objects/script.js
@@ -0,0 +1,19 @@
+// Объекты
+console.log("--- Объекты ---");
+
+// Задание №1 - Создать объект с полем 'notebook' равным 'MacBook' //
+let myObject = {     
+    notebook: 'MacBook'
+};
+  
+// Задание №2 - Добавить в объект из предыдущей задачи поле 'price', равное 1500 и поле currensy, равная 'dollar' //
+myObject.price = 1500;
+myObject.currency = 'dollar';
+
+// Задание №3 - Добавить поле details, которое будет содержать объект с полями model и color (значения этих полей задайте сами) //
+myObject.detailes = {};
+myObject.detailes.model='beauty';
+myObject.detailes.color='blue';
+
+console.log(myObject);
+
